62528cf0d7f9ccf9a956c8dc23ec6b44c71ed964
pkohn <phil@seekpanda.com>
2016-04-19 19:59:19 -0700

    Update main.js

--- main.js
+++ main.js
@@ -12,6 +12,9 @@ console.log(yourAnswer);
 
 //2. Declare a variable called 'name', and then log it to the console.
 
+var = 'name'
+console.log(name);
+
 //3. Declare two variables with number values. 
 //   Add, subtract, multiply and divide them.
 
